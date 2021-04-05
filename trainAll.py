a6a0865d34a12a55593d73b7bf35184e825a5028
dayonetv <59259102+dayonetv@users.noreply.github.com>
2021-04-05 18:08:37 +0300

    Update trainAll.py

--- trainAll.py
+++ trainAll.py
@@ -38,7 +38,7 @@ def parse_proto_example(proto):
   example = tf.io.parse_single_example(proto, keys_to_features)
   example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
   example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.uint8)
-  example['image'] = tf.image.resize(example['image'], tf.constant([RESIZE_TO, RESIZE_TO]))
+  example['image'] = tf.image.resize(example['image'], tf.constant([235, 235]))
   return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
 
 
@@ -52,9 +52,8 @@ def contrast(image, label):
 def brightness(image, label):
   return tf.image.adjust_brightness(image, delta=0.3), label
 
-#def crop(image, label):
-#  x = tf.image.resize(image, [300,300])
-#  return tf.image.random_crop(x, size = [224, 224, 3]), label
+def crop(image, label):
+  return tf.image.random_crop(x, size = [224, 224, 3]), label
 
 def create_dataset(filenames, batch_size):
   """Create dataset from tfrecords file
@@ -66,6 +65,7 @@ def create_dataset(filenames, batch_size):
     .cache()\
     .map(brightness)\
     .map(contrast)\
+    .mao(crop)\
     .batch(batch_size)\
     .prefetch(tf.data.AUTOTUNE)
 
@@ -80,8 +80,6 @@ def exp_decay(epoch,lr):
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
   x = tf.keras.layers.GaussianNoise(stddev = 0.6)(inputs)
-  x = tf.keras.layers.experimental.preprocessing.Resizing(235,235)(x)
-  x = tf.keras.layers.experimental.preprocessing.RandomCrop(224,224)(x)
   x = tf.keras.layers.experimental.preprocessing.RandomRotation(factor = 0.02)(x)
   model = EfficientNetB0(include_top=False, input_tensor = x, pooling = 'avg', weights='imagenet')
   model.trainable = False
